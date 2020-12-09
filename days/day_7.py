9ad1cfca8ec82949e91710d46145ceb41d2fba50
ferryrules <ferris@ferryrules.com>
2020-12-08 16:14:26 -0800

    day 7

--- days/day_7.py
+++ days/day_7.py
@@ -18,4 +18,25 @@ for color in holds_my_bag_colors:
 
 answers['first'] = len(set(holds_my_bag_colors))
 
+all_bags = {}
+def parse_line(line):
+    line = line.strip('.')
+    bag, contents = line.split(' bags contain ')
+    contents = contents.replace(' bags', '').replace(' bag', '').split(', ')
+    if contents[0] == 'no other':
+        contents = {}
+    contents = {' '.join(i.split()[1:]): int(i.split()[0]) for i in contents}
+    all_bags[bag] = contents
+
+for line in data:
+    parse_line(line)
+
+def num_inner_bags(color):
+    contents = all_bags[color]
+    if contents == {}:
+        return 0
+    return sum(count * (1 + num_inner_bags(bag)) for bag, count in contents.items())
+
+answers['second'] = num_inner_bags('shiny gold')
+
 print_answers(answers)
